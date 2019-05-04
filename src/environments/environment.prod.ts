2c64f3b57ebcb26381489de23eecb1d1c9a291b9
George Kuhl <gkuhl@verkuhl.com>
2019-05-04 18:22:09 -0400

    deployed with docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDBiB2CEw2aBdOUysBF0Wj3Le59kJS8x04",
+    authDomain: "georgekuhl-2fe0c.firebaseapp.com",
+    databaseURL: "https://georgekuhl-2fe0c.firebaseio.com",
+    projectId: "georgekuhl-2fe0c",
+    storageBucket: "georgekuhl-2fe0c.appspot.com",
+    messagingSenderId: "324870460759"
+  }
 };
